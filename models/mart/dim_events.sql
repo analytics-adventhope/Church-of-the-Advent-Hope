19e15b348e7cbad9980af3a9832c4e51ea508efc
analytics-adventhope <analytics@adventhope.org>
2025-10-03 00:37:16 -0400

    Create dim_events.sql

---
+++ models/mart/dim_events.sql
@@ -0,0 +1,12 @@
+select distinct generate_uuid() as event_sk,
+       id,
+       name,
+       description,
+       type,
+       event_format,
+       event_date,
+       event_time,
+       budget,
+       cost,
+       expected_revenue
+from {{ref('event_detail')}};
