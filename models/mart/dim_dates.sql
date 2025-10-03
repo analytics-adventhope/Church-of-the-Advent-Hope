abb72f02d97ae6af92cec699be851cd2943ed458
analytics-adventhope <analytics@adventhope.org>
2025-10-03 15:33:04 -0400

    Update dim_dates.sql

--- models/mart/dim_dates.sql
+++ models/mart/dim_dates.sql
@@ -1,11 +1,23 @@
-with days as (select date('2020-01-01') + interval x DAY as date
-              from unnest(generate_array(0, 18250)) as x  -- 50 years
-             )
-select distinct format_date('%Y%m%d', date) as date_sk,
-       date,
-       extract(dayofweek from date) as day_of_week,
-       extract(week from date) as week,
-       extract(month from date) as month,
-       extract(quarter from date) as quarter,
-       extract(year from date) as year
-from days
+with days as (
+    select date('2020-01-01') + interval x day as date
+    from unnest(generate_array(0, 18250)) as x  -- ~50 years
+),
+max_date as (
+    select max(end_date) as max_end_date
+    from {{ ref('fact_attendance') }}
+)
+select format_date('%Y%m%d', d.date) as date_sk,
+       d.date,
+       extract(dayofweek from d.date) as day_of_week,
+       extract(week from d.date) as week,
+       extract(month from d.date) as month,
+       extract(quarter from d.date) as quarter,
+       extract(year from d.date) as year,
+       case when date_diff(m.max_end_date, d.date, month) <= 3 then '3-month'
+            when date_diff(m.max_end_date, d.date, month) <= 6 then '6-month'
+            when date_diff(m.max_end_date, d.date, month) <= 12 then '12-month'
+            when date_diff(m.max_end_date, d.date, month) <= 24 then '24-month'
+            else 'all time'
+       end as time_bucket
+from days d
+cross join max_date m
