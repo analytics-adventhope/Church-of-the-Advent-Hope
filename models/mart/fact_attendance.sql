45c8701060cc82590cf0fee8a95a05cfefbc22f7
analytics-adventhope <analytics@adventhope.org>
2025-10-03 08:00:27 -0400

    Update fact_attendance.sql

--- models/mart/fact_attendance.sql
+++ models/mart/fact_attendance.sql
@@ -9,10 +9,10 @@ select m.member_sk,
        e.event_format,
        m.participant_type
 from {{ ref('event_detail') }} ed
-inner join {{ ref('dim_member') }} m
+inner join {{ ref('dim_members') }} m
         on cm.contact_id = m.contact_id
-inner join {{ ref('dim_event') }} e
+inner join {{ ref('dim_events') }} e
         on cm.event_id = e.id
-inner join {{ ref('dim_date') }} d
+inner join {{ ref('dim_dates') }} d
         on e.event_date = d.date
 where cm.attendance_indicator = true;
