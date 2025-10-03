56ce34f7173d33295bbfd4a46d12f43d7b54de79
analytics-adventhope <analytics@adventhope.org>
2025-10-03 08:41:40 -0400

    Update dim_dates.sql

--- models/mart/dim_dates.sql
+++ models/mart/dim_dates.sql
@@ -1,7 +1,7 @@
 with days as (select date('2020-01-01') + interval x DAY as date
               from unnest(generate_array(0, 18250)) as x  -- 50 years
              )
-select format_date('%Y%m%d', date) as date_sk,
+select distinct format_date('%Y%m%d', date) as date_sk,
        date,
        extract(dayofweek from date) as day_of_week,
        extract(week from date) as week,
