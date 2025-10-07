a677f2660743b9e6bd1de5fce63b401b6c5ef736
analytics-adventhope <analytics@adventhope.org>
2025-10-07 01:57:43 -0400

    Update dim_dates.sql

--- models/mart/dim_dates.sql
+++ models/mart/dim_dates.sql
@@ -11,11 +11,11 @@ select distinct date(d.date) as date,
        extract(month from d.date) as month,
        extract(quarter from d.date) as quarter,
        extract(year from d.date) as year,
-       case when date_diff(m.max_end_date, d.date, month) <= 3 then '0. 3-month'
-            when date_diff(m.max_end_date, d.date, month) <= 6 then '1. 6-month'
-            when date_diff(m.max_end_date, d.date, month) <= 12 then '2. 12-month'
-            when date_diff(m.max_end_date, d.date, month) <= 24 then '3. 24-month'
-            else 'all time'
+       case when date_diff(m.max_end_date, d.date, month) <= 3  then 'Months 0 - 3'
+            when date_diff(m.max_end_date, d.date, month) <= 6. then 'Months 4- 6'
+            when date_diff(m.max_end_date, d.date, month) <= 12 then 'Months 7 - 12'
+            when date_diff(m.max_end_date, d.date, month) <= 24 then 'Months 13 - 24'
+            else 'Months 25+'
        end as time_bucket
 from days d
 cross join max_date m
