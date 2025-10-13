14fc77b561b7d16ecd2a65e94a393a9c78ff0ebe
analytics-adventhope <analytics@adventhope.org>
2025-10-13 01:16:57 -0400

    Update fact_attendance.sql

--- models/mart/fact_attendance.sql
+++ models/mart/fact_attendance.sql
@@ -6,22 +6,14 @@ with primary_services as (
         m.contact_id,
 
         -- proportion of worship services attended in person
-        safe_divide(
-            count(distinct case when e.type = 'Worship Service'
-                                and ed.attendance_type = 'In-Person'
-                           then e.event_date end),
-            count(distinct case when e.type = 'Worship Service'
-                           then e.event_date end)
-        ) as in_person_prop,
+        count(distinct case when e.type = 'Worship Service'
+                             and ed.attendance_type = 'In-Person'
+                                then e.event_date end) as in_person_count,
 
         -- proportion of worship services attended at 11:30 AM
-        safe_divide(
-            count(distinct case when e.type = 'Worship Service'
-                                and e.event_time = '11:30 AM'
-                           then e.event_date end),
-            count(distinct case when e.type = 'Worship Service'
-                           then e.event_date end)
-        ) as second_service_prop
+        count(distinct case when e.type = 'Worship Service'
+                             and e.event_time = '11:30 AM'
+                                then e.event_date end)  as second_service_count
 
     from {{ ref('event_detail') }} ed
     inner join {{ ref('dim_members') }} m
@@ -46,8 +38,8 @@ select distinct
     m.first_name,
     m.last_name,
     m.participant_type,
-    p.in_person_prop,
-    p.second_service_prop,
+    p.in_person_count,
+    p.second_service_count,
     count(distinct e.event_date) over (partition by m.contact_id, d.time_bucket) as events_attended,
     count(distinct e.event_date) over (partition by d.time_bucket) as events_sum
 from {{ ref('event_detail') }} ed
